22888a0d3eb9c69fdc100aa8bbcdc61a9dee582c
none <none@ee.ee>
2020-09-16 13:10:05 +0300

    My Array compl

--- src/sptvr19myarray/App.java
+++ src/sptvr19myarray/App.java
@@ -13,14 +13,37 @@ import java.util.Random;
  */
 class App {
     public void run(){
-        System.out.println("---My Array---");
+        System.out.println("---My Array---\n"
+                + "Задание:");
+        System.out.println("Создайте массив из 20 случайных целых чисел.");
+        System.out.println("Вычислите среднее арифметическое элементов массива \n"
+                + "без учета минимального и максимального элементов массива.");
+        System.out.println("Решение: ");
+        System.out.println("Массив целых четных чисел");
         Random random = new Random();
+        int rnum;
+        rnum = 0;
+        int min = 100;
+        int max = 0;
+        int sum = 0;
         //Создаем массив целых четных чисел
         int[] numbers = new int[20];
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = random.nextInt(99-0+1)+0;
+            while (true) {
+                rnum = random.nextInt(99-0+1)+0;
+                if (rnum % 2 == 0) {
+                    numbers[i] = rnum;
+                    break;
+                }
+            }
+            if (numbers[i] < max)max = numbers[i];
+            if (numbers[i] > min)min = numbers[i];
+            sum += numbers[i];
             System.out.printf("%4d",numbers[i]);
         }
         System.out.println();
+        System.out.println("Среднее арифмитическое без учёта минимального " + min + " и максимального" + max);
+        System.out.printf((double)(sum-min-max)/(numbers.length - 2));
+        System.out.println("---End Array---");
     }
 }
